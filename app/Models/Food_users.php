670d322b5e784caf4fc957c4a513864985129811
WaheedNodesol <waheed@nodesol.com>
2023-03-23 09:05:45 -0400

    soft delete added

--- app/Models/Food_users.php
+++ app/Models/Food_users.php
@@ -4,10 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Food_users extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
     protected $table = 'food_users';
     protected $guarded = ['id'];
 }
